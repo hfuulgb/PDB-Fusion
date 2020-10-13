2caebc63b84ee32b02a7f3794c277c73e032794c
hfuulgb <mrlgb@live.com>
2020-10-13 18:38:15 +0800

    commit PDBP-Fusion 500-2500

--- data_preparation_dna.py
+++ data_preparation_dna.py
@@ -5,7 +5,14 @@
 
 import random
 
-def cutData(OrgSet,lens=14120,maxlen=600):
+from sklearn.model_selection import StratifiedKFold, KFold, StratifiedShuffleSplit
+from keras import backend as K
+from keras.utils import np_utils
+import numpy as np
+import os
+
+
+def cutData(OrgSet, lens=14120, maxlen=500):
     cutedSet = []
     i = -1
     for line in OrgSet:
@@ -15,77 +22,74 @@ def cutData(OrgSet,lens=14120,maxlen=600):
         # if i>lens:
         #     break
         temp = line.strip()
-        lenth=len(temp)
-        if lenth>maxlen:
-            temp=temp[0:maxlen]
-        if lenth<=maxlen:
-            temp=temp.ljust(maxlen,'Z')
+        lenth = len(temp)
+        if lenth > maxlen:
+            temp = temp[0:maxlen]
+        if lenth <= maxlen:
+            temp = temp.ljust(maxlen, "Z")
             # print(str(len(temp))+"          ")
         cutedSet.append(temp)
     return cutedSet
 
 
-def readfile(trueSetFile, falseSetFile):
+def readfile(trueSetFile, falseSetFile, maxlen):
     ret = False
     try:
-        OrgTrueSet = open(trueSetFile, 'r')  #打开文件
-        OrgFalseSet = open(falseSetFile, 'r')  #打开文件
+        OrgTrueSet = open(trueSetFile, "r")  # 打开文件
+        OrgFalseSet = open(falseSetFile, "r")  # 打开文件
     except IOError:
-        print('trueSetFile doesnot exist!')
+        print("trueSetFile doesnot exist!")
         return ret
 
-    
-
-    #逐行读取序列
-    cutedTrueSet = cutData(OrgTrueSet)
+    # 逐行读取序列
+    cutedTrueSet = cutData(OrgTrueSet, lens=14120, maxlen=maxlen)
     OrgTrueSet.close()
 
-
-    #逐行读取序列
-    cutedfalseSet = cutData(OrgFalseSet,14258)
+    # 逐行读取序列
+    cutedfalseSet = cutData(OrgFalseSet, lens=14258, maxlen=maxlen)
     OrgFalseSet.close()
 
-    #数据比例选择
+    # 数据比例选择
     return cutedTrueSet, cutedfalseSet
 
-#------------------------------------主函数---------------------------------------------
-
-from sklearn.model_selection import StratifiedKFold, KFold, StratifiedShuffleSplit
-from keras import backend as K
-from keras.utils import np_utils
-import numpy as np
-import os
 
 if __name__ == "__main__":
     path = "data/"
     if not os.path.exists(path):
         os.makedirs(path)
 
-    trueSet, falseSet = readfile(
-        'DNA/PDB186_P.txt',
-        'DNA/PDB186_N.txt')
-    # t_train_list = []
-    # tset = np.array(trueSet)
+    max_width = []
+    for i in np.arange(500, 2800, 50):
+        max_width.append(i)
 
-    print(len(trueSet))
-    print(len(falseSet))
-    # xglable = np.ones(1116)
-    # yglable = np.zeros(1116)
-    # xglable = np.ones(1116)
-    # yglable = np.zeros(1116)
-    # glabel = np.concatenate((xglable, yglable), axis=0)
-    # # y_train =np.array(np.array(glabel))
-    with open(path+'DNA_Pading_600_PDB186', mode='w',
-              encoding='utf-8') as myfile:
-        myfile.write('\n'.join(trueSet))
-        myfile.write('\n')
-        myfile.write('\n'.join(falseSet))
-        myfile.write('\n')
+    for maxlength in max_width[:-1]:
 
-    with open(path+'class_PDB186', mode='w', encoding='utf-8') as myfile:
-        # for indexi in range(2796):
-        for indexi in range(len(trueSet)):
-            myfile.write('1' + '\n')
-        # for indexi in range(2796):
-        for indexi in range(len(falseSet)):
-            myfile.write('0' + '\n')
+        trueSet, falseSet = readfile(
+            "DNA/PDB14189_P.txt", "DNA/PDB14189_N.txt", maxlen=maxlength
+        )
+        # t_train_list = []
+        # tset = np.array(trueSet)
+
+        print(len(trueSet))
+        print(len(falseSet))
+        # xglable = np.ones(1116)
+        # yglable = np.zeros(1116)
+        # xglable = np.ones(1116)
+        # yglable = np.zeros(1116)
+        # glabel = np.concatenate((xglable, yglable), axis=0)
+        # # y_train =np.array(np.array(glabel))
+        with open(
+            path + "DNA-Pading-DB14189-" + str(maxlength), mode="w", encoding="utf-8"
+        ) as myfile:
+            myfile.write("\n".join(trueSet))
+            myfile.write("\n")
+            myfile.write("\n".join(falseSet))
+            myfile.write("\n")
+
+        with open(path + "class_test_PDB14189", mode="w", encoding="utf-8") as myfile:
+            # for indexi in range(2796):
+            for indexi in range(len(trueSet)):
+                myfile.write("1" + "\n")
+            # for indexi in range(2796):
+            for indexi in range(len(falseSet)):
+                myfile.write("0" + "\n")
